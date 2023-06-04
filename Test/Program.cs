dece55f674e8277f71901665e9ab4da3dfb14fa5
Admin110 <Admin110@VTC15>
2023-06-04 20:17:14 +0400

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("greq bnakan tiv 50-ic 70 mijakayqum");
+
+            int tiv = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 50; i <= 70; i++)
+            {
+
+
+                tiv = i;
+                if (tiv % 2 == 0)
+                {
+                    tiv = tiv / 2;
+                }
+                else if (tiv % 2 != 0)
+                {
+                    tiv = (tiv * 3 + 2) / 2;
+                }
+               
+                    Console.WriteLine(tiv);
+
+                
+            }
+            
+            
+        }
+      }
+    }
